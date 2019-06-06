5d56071f4f9d16cc5bf77e601b96ce592550e817
Ilya Nozhkin <nozhkin.ii@gmail.com>
2019-06-06 23:11:47 +0300

    Fix Windows build

--- Console/main.cpp
+++ Console/main.cpp
@@ -4,7 +4,6 @@
 #include <QtWebEngine>
 
 using namespace std;
-using namespace HellGateBot;
 
 int main(int argc, char *argv[])
 {
@@ -15,8 +14,8 @@ int main(int argc, char *argv[])
     hellGateBot.setPatient(u8"НОЖКИН", u8"ИЛЬЯ", u8"ИГОРЕВИЧ", u8"10/10/1998");
     hellGateBot.setDoctorType(u8"ТЕРАПЕВТ");
     hellGateBot.setDoctor(u8"МЕДЯНОВА");
-    hellGateBot.setDate(SimpleDate(07, 06, 2019));
-    hellGateBot.setTime(SimpleTime(00, 00), SimpleTime(23, 00));
+    hellGateBot.setDate(HellGateBot::SimpleDate(07, 06, 2019));
+    hellGateBot.setTime(HellGateBot::SimpleTime(00, 00), HellGateBot::SimpleTime(23, 00));
     hellGateBot.obtain("ticket.pdf");
 
     return 0;
